38090ba76a139c775a7aabb011ffeba4310d2fd0
CosmicAnon <wescosmic@gmail.com>
2023-06-07 11:49:21 +0545

    aframe online

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Intro from './pages/introPage';
+// import Welcome from './pages/welcomePage';
 import CesiumViewer from './CesiumViewer';
+import AframeScene from './aframeScene';
 
 function App() {
   return (
-    <div className="App">
-      <CesiumViewer />
-    </div>
+
+
+
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<CesiumViewer />} />
+        <Route exact path="/aframe" element={<AframeScene />} />
+        <Route element={<CesiumViewer />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
