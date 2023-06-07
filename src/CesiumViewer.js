78ce6efbd7c49d9143e94bbf04cfd1a425563521
CosmicAnon <wescosmic@gmail.com>
2023-06-07 10:54:46 +0545

    Create CesiumViewer.js

---
+++ src/CesiumViewer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Viewer } from 'cesium';
+
+class CesiumViewer extends React.Component {
+  componentDidMount() {
+    // Initialize the Cesium Viewer in the componentDidMount lifecycle method
+    this.viewer = new Viewer(this.cesiumContainer);
+  }
+
+  componentWillUnmount() {
+    // Destroy the viewer when the component is unmounted
+    if (this.viewer) {
+      this.viewer.destroy();
+    }
+  }
+
+  render() {
+    // Render the container that will be used for the viewer
+    return (
+      <div
+        className="cesiumContainer"
+        ref={element => { this.cesiumContainer = element; }}
+        style={{ width: '100%', height: '100%', position: 'absolute' }}
+      />
+    );
+  }
+}
+
+export default CesiumViewer;
