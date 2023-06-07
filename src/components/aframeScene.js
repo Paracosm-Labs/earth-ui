d5becc0484db686258d43aa26217b9cfd850b0e8
CosmicAnon <wescosmic@gmail.com>
2023-06-07 15:12:58 +0545

    updates

--- src/components/aframeScene.js
+++ src/components/aframeScene.js
@@ -3,13 +3,18 @@ import 'aframe';
 
 class AframeScene extends React.Component {
   render() {
+    // Get the location from the URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const location = urlParams.get('location');
+
+    // Get the URL of the 360-degree image for this location
+    // This could be a static URL, or it could be dynamically generated based on the location
+    const imageUrl = `/environments/${location}.jpg`;
+
     return (
       <a-scene>
-        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-        <a-sky color="#ECECEC"></a-sky>
+        <a-sky src={imageUrl}></a-sky>
+        {/* Add other elements to the scene here */}
       </a-scene>
     );
   }
