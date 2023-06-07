dfd630bef6612b7ac1c7fe55b6ada918dadee214
CosmicAnon <wescosmic@gmail.com>
2023-06-07 15:50:15 +0545

    updates

--- src/components/aframeScene.js
+++ src/components/aframeScene.js
@@ -11,17 +11,23 @@ class AframeScene extends React.Component {
     const imageUrl = `/environments/${location}.jpg`;
 
     return (
-      <a-scene>
-        <a-sky src={imageUrl}></a-sky>
-        {/* Add a welcome message */}
-        <a-text
-          value={`Welcome to ${location}`}
-          position="0 2.5 -3"
-          align="center"
-          color="#FFFFFF"
-        ></a-text>
-        {/* Add other elements to the scene here */}
-      </a-scene>
+      <div>
+        <a-scene>
+          <a-sky src={imageUrl}></a-sky>
+          {/* Add a welcome message */}
+          <a-text
+            value={`Welcome to ${location}`}
+            position="0 2.5 -3"
+            align="center"
+            color="#323232"
+          ></a-text>
+          {/* Add other elements to the scene here */}
+        </a-scene>
+        {/* Add a link back to the globe */}
+        <a href="/" className="mb-5" style={{ position: 'absolute', bottom: '50px',left:'40%', color: '#434343', textDecoration: 'none' }}>
+          <h1 className="text-center">Return to Orbital Station</h1>
+        </a>
+      </div>
     );
   }
 }
