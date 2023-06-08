272b14b3bac546d36d90a960a445660718c0b452
CosmicAnon <wescosmic@gmail.com>
2023-06-08 16:03:12 +0545

    Update hud.js

--- src/components/hud.js
+++ src/components/hud.js
@@ -5,12 +5,12 @@ import { BounceLoader } from 'react-spinners';
 const Hud = () => {
   const [resources, setResources] = useState(0);
   const [units, setUnits] = useState([
-    { id: 1, name: 'Unit 1', image: 'https://dummyimage.com/120x100/000/777&text=Unit1' },
-    { id: 2, name: 'Unit 2', image: 'https://dummyimage.com/120x100/000/777&text=Unit2' },
-    { id: 3, name: 'Unit 3', image: 'https://dummyimage.com/120x100/000/777&text=Unit3' },
-    { id: 4, name: 'Unit 4', image: 'https://dummyimage.com/120x100/000/777&text=Unit4' },
-    { id: 5, name: 'Unit 5', image: 'https://dummyimage.com/120x100/000/777&text=Unit5' },
-    { id: 6, name: 'Unit 6', image: 'https://dummyimage.com/120x100/000/777&text=Unit6' },
+    { id: 1, name: 'Unit 1', image: 'https://dummyimage.com/100x60/000/777&text=Unit1' },
+    { id: 2, name: 'Unit 2', image: 'https://dummyimage.com/100x60/000/777&text=Unit2' },
+    { id: 3, name: 'Unit 3', image: 'https://dummyimage.com/100x60/000/777&text=Unit3' },
+    { id: 4, name: 'Unit 4', image: 'https://dummyimage.com/100x60/000/777&text=Unit4' },
+    { id: 5, name: 'Unit 5', image: 'https://dummyimage.com/100x60/000/777&text=Unit5' },
+    { id: 6, name: 'Unit 6', image: 'https://dummyimage.com/100x60/000/777&text=Unit6' },
     // Add more dummy units here
   ]);
   const [selectedUnit, setSelectedUnit] = useState(null);
@@ -33,14 +33,14 @@ const Hud = () => {
     <div className="hud text-center mx-1 h-100">
       <div className="minimap mt-3 p-5 pt-3">
         {/* Render minimap here */}
-        <img src="/labs-logo.png" alt="Minimap" className="rounded-circle" width="100px"/><br/>
+        <img src="/sorrel-logo.png" alt="Minimap" className="rounded-circle" width="100px"/><br/>
         Earth Communications Uplink
       </div>
       <div className="resource-counter">
         Resources: {resources}
       </div>
       <div className="mt-3">
-        <Button onClick={() => setShowWallet(true)}>Show Wallet</Button>
+        <Button className="btn btn-outline-success" onClick={() => setShowWallet(true)}>Show Wallet</Button>
         <Offcanvas show={showWallet} onHide={() => setShowWallet(false)} placement="end" title="Wallet">
           {loading && 
             <div className="spinner-container">
@@ -65,7 +65,7 @@ const Hud = () => {
           <div className="col mt-2" key={unit.id} onClick={() => setSelectedUnit(unit)}>
           <button className="btn btn-outline-secondary w-100">
             <img src={unit.image} alt={unit.name} className="" />
-            <p>{unit.name}</p>
+            <small>{unit.name}</small>
           </button>
           </div>
         ))}
